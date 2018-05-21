4032773f5e97fee0405c7b54aac7a93d9940b6ff
fujix129 <minami_hyoufk@yahoo.co.jp>
2018-05-21 13:50:41 +0900

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Home from './container/home';
+import Frends from './container/frends';
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/frends' component={Frends} />
+    </div>
+  </BrowserRouter>
+)
 
 export default App;
---
+++ src/container/frends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Frends = () => {
+  return (
+    <div>
+      <h1>Frends</h1>
+    </div>
+  )
+}
+
+export default Frends
