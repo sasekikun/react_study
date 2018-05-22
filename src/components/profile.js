ee009d8e8aa94037e773aa64894e73fa96847f0c
fujix129 <minami_hyoufk@yahoo.co.jp>
2018-05-22 14:44:46 +0900

    add stylesheets

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,13 +3,15 @@ import { compose } from 'recompose';
 
 import Header from '../components/Header';
 
+import '../stylesheets/profile.css'
+
 const enhance = compose()
 
 const Profile = enhance(props => {
   const { img, name, type, age, sex, link } = props
   return (
-    <div style={{margin: '30px',padding: '10px', width: '200px', border:'solid'}}>
-      <img src={img} style={{width: '100%', height: 'auto'}} />
+    <div className="profileCard">
+      <img src={img} className="profileImg" />
       <p>名前：{name}</p>
       <p>種別：{type}</p>
       <p>年齢：{age}</p>
