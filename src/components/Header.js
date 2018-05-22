a367529af43f80aec5dec86b163c025764d1f61e
fujix129 <minami_hyoufk@yahoo.co.jp>
2018-05-22 17:16:54 +0900

    検索ページ追加、ヘッダのスタイル修正

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Header = () => {
   return (
     <div>
-      <p><a href="/">home</a></p>
-      <p><a href="/friends">friends</a></p>
+      <p style={{display:'inline-block', margin:'10px'}}><a href="/">home</a></p>
+      <p style={{display:'inline-block', margin:'10px'}}><a href="/friends">friends</a></p>
+      <p style={{display:'inline-block', margin:'10px'}}><a href="/search">search</a></p>
     </div>
   )
 }
