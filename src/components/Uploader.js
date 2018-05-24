d00e22587f55244efc68a8ca7368ae3a3ba994b6
fujix129 <minami_hyoufk@yahoo.co.jp>
2018-05-24 11:04:01 +0900

    add uploader

---
+++ src/components/Uploader.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+var createObjectURL = (window.URL || window.webkitURL).createObjectURL || window.createObjectURL;
+
+class PositionedSnackbar extends React.Component {
+  state = {
+    image_src: "",
+    open: false,
+    vertical: null,
+    horizontal: null,
+  };
+
+  handleChangeFile = e => {
+      var files = e.target.files;
+      var image_url = createObjectURL(files[0]);
+      this.setState({image_src: image_url});
+
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="file" ref="file" onChange={this.handleChangeFile} />
+        <br />
+        <img src={this.state.image_src} alt={this.state.image_src} style={{width: '200px', height:'auto'}} />
+      </div>
+    );
+  }
+}
+
+export default PositionedSnackbar;
--- src/container/Registration.js
+++ src/container/Registration.js
@@ -5,6 +5,7 @@ import TextField from 'material-ui/TextField'
 import Header from '../components/Header'
 import ApplyButtons from '../components/ApplyButtons'
 import Paragraph from '../components/Paragraph'
+import Uploader from '../components/Uploader'
 
 const applyACtion = (e) => {
   console.log("登録するよー")
@@ -16,6 +17,8 @@ const Home = () => {
       <Header />
       <div style={{margin: '30px'}}>
         <h1>登録ページ</h1>
+        <Paragraph text="画像" />
+        <Uploader />
         <Paragraph text="名前" />
         <TextField />
         <Paragraph text="種別" />
